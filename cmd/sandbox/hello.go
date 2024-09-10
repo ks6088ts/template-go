97fb64851ef0e6d15ff5ca7a681c44abbf370441
ks6088ts <ks6088ts@gmail.com>
2024-09-10 22:20:58 +0900

    modify hello command

--- cmd/sandbox/hello.go
+++ cmd/sandbox/hello.go
@@ -19,7 +19,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
-package cmd
+package sandbox
 
 import (
 	"fmt"
@@ -30,15 +30,10 @@ import (
 // helloCmd represents the hello command
 var helloCmd = &cobra.Command{
 	Use:   "hello",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "SandBox Hello Command",
+	Long:  `This is a sandbox command.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("hello called")
+		fmt.Println("sandbox hello called")
 	},
 }
 
